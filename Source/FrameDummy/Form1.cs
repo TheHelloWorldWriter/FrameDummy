1c5b721c41826f858eb8acdb404676c87a07f150
TechAurelian <dev@techaurelian.com>
2026-04-30 11:39:11 +0300

    Add initial project files for FrameDummy modern .NET 10 rewrite

---
+++ Source/FrameDummy/Form1.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2013-2026 The Hello World Writer (https://www.thehelloworldwriter.com).
+// Licensed under the MIT License. See the LICENSE file in the project root for more information.
+
+namespace FrameDummy;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/FrameDummy/Program.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2013-2026 The Hello World Writer (https://www.thehelloworldwriter.com).
+// Licensed under the MIT License. See the LICENSE file in the project root for more information.
+
+namespace FrameDummy;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
