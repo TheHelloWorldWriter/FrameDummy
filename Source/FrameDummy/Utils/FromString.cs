b123fb7828620dc87acfc02110f7fcff0e3d7ad7
TechAurelian <dev@techaurelian.com>
2026-05-01 23:03:41 +0300

    Update copyright year in multiple files

--- Source/FrameDummy/Utils/FromString.cs
+++ Source/FrameDummy/Utils/FromString.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2026 The Hello World Writer (https://www.thehelloworldwriter.com).
+// Copyright (c) 2013-present The Hello World Writer (https://www.thehelloworldwriter.com).
 // Licensed under the MIT License. See the LICENSE file in the project root for more information.
 
 using System.Diagnostics.CodeAnalysis;
